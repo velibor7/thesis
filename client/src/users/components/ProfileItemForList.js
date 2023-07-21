e008f72fc66959e108fe88d26dcf2a0d55ea44da
Velibor Vasiljevic <v.vasiljevic@vegait.rs>
2023-07-21 16:59:34 +0200

    profiles fix;

--- client/src/users/components/ProfileItemForList.js
+++ client/src/users/components/ProfileItemForList.js
@@ -76,34 +76,34 @@ const ProfileItemForList = (props) => {
   return (
     <>
       <div className="profile__item">
-        <Link to={`/profiles/${props.item.id}`}>
-          <h4 className="profile__item__firstName">{props.item.username}</h4>
+        <Link to={`/profiles/${props?.item.id}`}>
+          <h4 className="profile__item__firstName">{props?.item.username}</h4>
         </Link>
         <div className="profile__item__info">
           <p className="profile__item__lastName">
-            Last name: {props.item.lastName}
+            Last name: {props?.item.lastName}
           </p>
-          <p className="profile__item__gender">Email: {props.item.email}</p>
-          <p className="profile__item__biography">Bio: {props.item.bio}</p>
+          <p className="profile__item__gender">Email: {props?.item.email}</p>
+          <p className="profile__item__biography">Bio: {props?.item.bio}</p>
           <p className="profile__item__biography">
-            Posts: {props.item.post_count}
-          </p>
-          <p className="profile__item__biography">
-            Follower: {props.item.follower_count}
+            Posts: {props?.item.post_count}
           </p>
           <p className="profile__item__biography">
-            Following: {props.item.following_count}
+            Follower: {props?.item.follower_count}
+          </p>
+          <p className="profile__item__biography">
+            Following: {props?.item.following_count}
           </p>
         </div>
         {auth.userId && (
           <div className="profile-item__actions">
             {/* {!followingIds.includes(props?.item.id) ? ( */}
             {!props?.currentUserFollowing.includes(props?.item.id) ? (
-              <Button info onClick={() => followUser(props.item.id)}>
+              <Button info onClick={() => followUser(props?.item.id)}>
                 Follow
               </Button>
             ) : (
-              <Button danger onClick={() => unfollowUser(props.item.id)}>
+              <Button danger onClick={() => unfollowUser(props?.item.id)}>
                 Unfollow
               </Button>
             )}
